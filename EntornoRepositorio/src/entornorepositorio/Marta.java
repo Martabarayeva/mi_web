fede01e4d56a8fb679b271289e31625920c08faa
marta <marta@equipo06>
2017-03-22 14:04:43 +0100

    Programa Marta

---
+++ EntornoRepositorio/src/entornorepositorio/Marta.java
@@ -0,0 +1,13 @@
+
+package entornorepositorio;
+
+
+public class Marta {
+
+  
+    public static void main(String[] args) {
+        System.out.println("El porcentaje de alumnos que quiere que gane el barsa es:90%");
+       
+    }
+    
+}
